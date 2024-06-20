56e7da9325384458e7a7ae3839727af4590f84be
Hongrui Fang <chfanghr@gmail.com>
2024-06-21 00:32:13 +0800

    add tests for v1 `ScriptPurpose` instances

---
+++ src/Tests/V1/ScriptPurposeInstances-test.ts
@@ -0,0 +1,72 @@
+// Tests for the instances for `ScriptPurpose`
+import * as V1 from "../../Lib/V1.js";
+
+import { describe, it } from "node:test";
+
+import * as TestUtils from "../TestUtils.js";
+import fc from "fast-check";
+
+import * as TestStakingCredential from "../V1/StakingCredentialInstances-test.js";
+import * as TestTxOutRef from "../V1/TxOutRefInstances-test.js";
+import * as TestValue from "../V1/ValueInstances-test.js";
+import * as TestDCert from "../V1/DCertInstances-test.js";
+
+export function fcScriptPurpose(): fc.Arbitrary<V1.ScriptPurpose> {
+  const { scriptPurpose } = fc.letrec((tie) => ({
+    scriptPurpose: fc.oneof(
+      {},
+      tie("Minting"),
+      tie("Spending"),
+      tie("Rewarding"),
+      tie("Certifying"),
+    ),
+    Minting: fc.record({
+      name: fc.constant("Minting"),
+      fields: TestValue.fcCurrencySymbol(),
+    }),
+    Spending: fc.record({
+      name: fc.constant("Spending"),
+      fields: TestTxOutRef.fcTxOutRef(),
+    }),
+    Rewarding: fc.record({
+      name: fc.constant("Rewarding"),
+      fields: TestStakingCredential.fcStakingCredential(),
+    }),
+    Certifying: fc.record({
+      name: fc.constant("Certifying"),
+      fields: TestDCert.fcDCert(),
+    }),
+  }));
+
+  return scriptPurpose as fc.Arbitrary<V1.ScriptPurpose>;
+}
+
+describe("ScriptPurpose tests", () => {
+  describe("Eq ScriptPurpose tests", () => {
+    it(`eq is not neq property based tests`, () => {
+      fc.assert(
+        fc.property(fcScriptPurpose(), fcScriptPurpose(), (l, r) => {
+          TestUtils.negationTest(V1.eqScriptPurpose, l, r);
+        }),
+      );
+    });
+  });
+  describe("Json ScriptPurpose tests", () => {
+    it(`toJson/fromJson property based tests`, () => {
+      fc.assert(
+        fc.property(fcScriptPurpose(), (data) => {
+          TestUtils.toJsonFromJsonRoundTrip(V1.jsonScriptPurpose, data);
+        }),
+      );
+    });
+  });
+  describe("IsPlutusData ScriptPurpose tests", () => {
+    it(`toData/fromData property based tests`, () => {
+      fc.assert(
+        fc.property(fcScriptPurpose(), (data) => {
+          TestUtils.isPlutusDataRoundTrip(V1.isPlutusDataScriptPurpose, data);
+        }),
+      );
+    });
+  });
+});
